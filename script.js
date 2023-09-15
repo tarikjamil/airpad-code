605ceeabdd7e67ba34f7fac8d281d685fc147c94
tarikjamil <tarikjamill@gmail.com>
2023-09-15 17:33:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,18 +8,39 @@ function pageLoad() {
     opacity: 1,
     ease: "Quint.easeOut",
     duration: 0.5,
-  }).from(
+  });
+
+  // Add a label to mark the starting point of simultaneous animations
+  tl.add("loadingAnimationsStart");
+
+  // Add the 'loading' animation and set its position to the label
+  tl.from(
     "[animation=loading]",
     {
       y: "20rem",
-      opacity: 0,
+      opacity: "0",
       stagger: { each: 0.1, from: "start" },
       ease: "Quint.easeOut",
       duration: 1,
     },
     "loadingAnimationsStart"
-  );
+  ); // <-- position parameter set to the label
+
+  // Add the 'loading-reverse' animation and set its position to the label
+  tl.from(
+    "[animation=loading-reverse]",
+    {
+      y: "-20rem",
+      opacity: "0",
+      stagger: { each: 0.1, from: "start" },
+      ease: "Quint.easeOut",
+      duration: 1,
+    },
+    "loadingAnimationsStart"
+  ); // <-- position parameter set to the label
 }
+
+pageLoad();
 
 $(document).ready(function () {
   var $navbar = $(".navbar");
