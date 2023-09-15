4e6916fcefa37d0717eaec4c60d0d7cf310a8c36
tarikjamil <tarikjamill@gmail.com>
2023-09-15 17:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,80 +8,53 @@ function pageLoad() {
     opacity: 1,
     ease: "Quint.easeOut",
     duration: 0.5,
-  });
-
-  // Add a label to mark the starting point of simultaneous animations
-  tl.add("loadingAnimationsStart");
-
-  // Add the 'loading' animation and set its position to the label
-  tl.from(
-    "[animation=loading]",
-    {
-      y: "20rem",
-      opacity: "0",
-      stagger: { each: 0.1, from: "start" },
-      ease: "Quint.easeOut",
-      duration: 1,
-    },
-    "loadingAnimationsStart"
-  ); // <-- position parameter set to the label
-
-  // Add the 'loading-reverse' animation and set its position to the label
-  tl.from(
-    "[animation=loading-reverse]",
-    {
-      y: "-20rem",
-      opacity: "0",
-      stagger: { each: 0.1, from: "start" },
-      ease: "Quint.easeOut",
-      duration: 1,
-    },
-    "loadingAnimationsStart"
-  ); // <-- position parameter set to the label
+  })
+    .from(
+      "[animation=loading]",
+      {
+        y: "20rem",
+        opacity: 0,
+        stagger: { each: 0.1, from: "start" },
+        ease: "Quint.easeOut",
+        duration: 1,
+      },
+      "loadingAnimationsStart"
+    )
+    .from(
+      "[animation=loading-reverse]",
+      {
+        y: "-20rem",
+        opacity: 0,
+        stagger: { each: 0.1, from: "start" },
+        ease: "Quint.easeOut",
+        duration: 1,
+      },
+      "loadingAnimationsStart"
+    );
 }
 
-pageLoad();
-
-$("[animation=fade-overflow]").each(function (index) {
-  let target = $(this);
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $(this),
-      start: "top bottom -=200",
-    },
-  });
-
-  tl.from(
-    target,
-    {
+function scrollFadeAnimation(selector) {
+  $(selector).each(function () {
+    let target = $(this);
+    gsap.from(target, {
       y: "20rem",
       opacity: 0,
       ease: "Quint.easeOut",
       duration: 1,
-    },
-    0
-  );
-});
-
-$("[animation=fade]").each(function (index) {
-  let target = $(this);
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: $(this),
-      start: "top bottom -=200",
-    },
+      scrollTrigger: {
+        trigger: target,
+        start: "top bottom -=200",
+        // markers: true // Uncomment for debugging
+      },
+    });
   });
+}
 
-  tl.from(
-    target,
-    {
-      y: "20rem",
-      opacity: 0,
-      ease: "Quint.easeOut",
-      duration: 1,
-    },
-    0
-  );
+// On Document Ready
+$(document).ready(function () {
+  pageLoad();
+  scrollFadeAnimation("[animation=fade-overflow]");
+  scrollFadeAnimation("[animation=fade]");
 });
 
 $(document).ready(function () {
